188c79545c327a8a4d69799fbbd93b499acf0f7a
TrungHieu <harrypotter12g@gmail.com>
2017-06-22 15:40:35 +0700

    project 1 by trunghieu

---
+++ vendor/bundle/ruby/2.3.0/gems/nokogiri-1.8.0/ext/nokogiri/tmp/x86_64-pc-linux-gnu/ports/libxml2/2.9.4/libxml2-2.9.4/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/trunghieu/Documents/rails pull/Project1/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.8.0/ports/x86_64-pc-linux-gnu/libxml2/2.9.4/lib"
+XML2_LIBS="-lxml2 -lz     -lm "
+XML2_INCLUDEDIR="-I/home/trunghieu/Documents/rails pull/Project1/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.8.0/ports/x86_64-pc-linux-gnu/libxml2/2.9.4/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
