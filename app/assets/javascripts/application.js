e3c36c83fa17bee883e57116769bfe49bfe122d7
TrungHieu <harrypotter12g@gmail.com>
2017-06-23 13:12:41 +0700

    Heroku branch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,31 +3,4 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
-jQuery(document).ready(function($) {
-  $('.new_comment').submit(function(event) {
-      event.preventDefault();
-      var params = $(this).serialize();
-      var form = $(this);
-       $.ajax({
-         url: form.attr('action'),
-         type: 'post',
-         dataType: 'json',
-         data: params,
-       })
-       .done(function(response) {
-        if(response.status == 'success'){
-          $('#comments-section').append(response.html);
-          $('#comment_content').val('');
-        }else {
 
-        }
-        })
-       .fail(function() {
-         console.log("error");
-       })
-       .always(function() {
-         console.log("complete");
-       });
-
-  });
-});
